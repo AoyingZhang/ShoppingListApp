c206b63657e91367f54102dd01d137ab2a381060
Aoying Zhang <107318892+AoyingZhang@users.noreply.github.com>
2023-05-11 23:22:18 +0000

    bugs troubleshoot

--- index.js
+++ index.js
@@ -13,58 +13,52 @@ const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
-addButtonEl.addEventListener("click", function() {
-    
-        let inputValue = inputFieldEl.value
-    if (inputValue != "") {
-        //push new value to database
-        push(shoppingListInDB, inputValue)
-        
-        clearInputFieldEl()
-        
-        appendItemToShoppingListEl(inputValue)
-    }
+addButtonEl.addEventListener("click", function () {
+    let inputValue = inputFieldEl.value
+
+    push(shoppingListInDB, inputValue)
+
+    clearInputFieldEl()
 })
-//update array from database, snapshot is the new data
-onValue(shoppingListInDB, function(snapshot){
-    if(snapshot.exists()){
-        //get array of array(key, value)
-        let itemArray = Object.entries(snapshot.val())
+
+onValue(shoppingListInDB, function (snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+
         clearShoppingListEl()
-        for(let i=0; i<itemArray.length; i++){
-            let currentItem=itemArray[i]
-            let currentKey=currentItem[0]
-            let currentValue=currentItem[1]
+
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+
             appendItemToShoppingListEl(currentItem)
         }
-    }
-    else{
+    } else {
         shoppingListEl.innerHTML = "No items here... yet"
     }
 })
 
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
 
 function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
-function clearShoppingListEl(){
-    shoppingListEl.innerHTML=''
-}
-
 function appendItemToShoppingListEl(item) {
-    //create element with tag
     let itemID = item[0]
-    let itemValue=item[1]
+    let itemValue = item[1]
 
     let newEl = document.createElement("li")
-    //set text inside
+
     newEl.textContent = itemValue
 
-    newEl.addEventListener("click", function(){
+    newEl.addEventListener("click", function () {
         let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+
         remove(exactLocationOfItemInDB)
     })
-    //choose where to put
+
     shoppingListEl.append(newEl)
-    
 }
